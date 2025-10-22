f84da2b537e32d4e0d8c17af26cf8ee44c1d1543
mohanned <MohannedAshraf GitHub>
2025-10-22 13:30:44 +0300

    main features

---
+++ lib/core/utils/app_icons.dart
@@ -0,0 +1,4 @@
+abstract class AppIcons {
+  static const String _baseUrl = "assets/image/";
+  static const String logo = '${_baseUrl}logo.svg';
+}
---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,4 @@
+abstract class AppImages {
+  static const String _baseUrl = "assets/images/";
+  static const String logo = '${_baseUrl}logo.png';
+}
